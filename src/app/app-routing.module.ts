9d092bcdf4c53e18cc659fdc8e14bde8890b21ee
DevdattaG <devdatta.godbole777@gmail.com>
2021-03-07 23:00:40 +0530

    Adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { InfoComponent } from './components/info/info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'how-it-works', component: InfoComponent },
+  { path: 'user-list', component: UserListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
